b16e64957d60a011de24bb391d469e2842f805b5
Sudip Giri <girisudip12@gmail.com>
2022-06-26 21:41:58 +0545

    fixed components to reuse over

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
 
 const App = () => {
-  const activeMenu=false
+  const activeMenu = false
   return (
     <div>
       <BrowserRouter>
@@ -14,22 +14,59 @@ const App = () => {
           <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
             <TooltipComponent content="Settings" position="Top">
               <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-              style={{ background:'blue', borderRadius:'50%'}}>
+                style={{ background: 'blue', borderRadius: '50%' }}>
                 <FiSettings />
               </button>
 
             </TooltipComponent>
 
           </div>
-          {activeMenu?(
+          {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div>
-          ):
-          (<div className="w-0 dark:bg-secondary-darl-bg">
-            Sidebar w-0
-          </div>)}
-          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu?'md:ml-72':'flex:2'}`}>
-            
+          ) :
+            (<div className="w-0 dark:bg-secondary-darl-bg">
+              Sidebar w-0
+            </div>)}
 
+          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex:2'}`}>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              Navbar
+            </div>
+
+          </div>
+          <div>
+            <Routes>
+              {/* DASHBOARD */}
+              <Route path="/" element="Ecommerce" />
+              <Route path="/eommerce" element="Ecommerce" />
+              {/* PAGES */}
+              <Route path="/orders" element="Orders" />
+              <Route path="/employees" element="Employees" />
+              <Route path="/customers" element="Customers" />
+              {/* Apps Navigation */}
+              <Route path="/kanban" element="Kanban" />
+              <Route path="/editor" element="Editor" />
+              <Route path="/calender" element="Calender" />
+              <Route path="/color-picker" element="ColorPicker" />
+              {/* charts */}
+              <Route path="/line" element="Line" />
+              <Route path="/area" element="Area" />
+              <Route path="/bar" element="Bar" />
+              <Route path="/pie" element="Pie" />
+              <Route path="/financial" element="Financial" />
+              <Route path="/color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="Pyramid" />
+              <Route path="/stacked" element="Stacked" />
+
+
+
+
+
+
+
+
+
+            </Routes>
           </div>
 
         </div>
